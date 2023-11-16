bb30d72c1608e7b03a7a0aef34367bd9fa404c26
Sisindry M <24821639+sisindrymedagam@users.noreply.github.com>
2023-11-16 23:05:21 +0530

    cookie handler

--- projects/ngx-cookie-handler-ssr/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-cookie-handler-ssr
- */
-
-export * from './lib/ngx-cookie-handler-ssr.service';
-export * from './lib/ngx-cookie-handler-ssr.component';
--- projects/ngx-cookie-handler/src/public-api.ts
+++ projects/ngx-cookie-handler/src/public-api.ts
@@ -2,5 +2,11 @@
  * Public API Surface of ngx-cookie-handler
  */
 
-export * from './lib/ngx-cookie-handler.service';
-export * from './lib/ngx-cookie-handler.component';
+export * from './lib/tokens';
+export * from './lib/cookie.service';
+export * from './lib/cookie.module';
+export * from './lib/cookie.model';
+export * from './lib/cookie-options.provider';
+export * from './lib/cookie-writer.service';
+export * from './lib/cookie.factory';
+export * from './lib/utils';
